cbf2935b92fcc54447fc12e3565d93e0f8874695
omid.eghaneyan <oeghaneyan@gmail.com>
2022-09-29 19:41:26 -0700

    initial main commit

---
+++ main.tf
@@ -0,0 +1,127 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "hcp_packer_iteration" "aws-ubuntu" {
+  bucket_name = ore-ubuntu-image
+  channel     = Production
+}
+
+data "hcp_packer_image" "aws-ubuntu" {
+  bucket_name    = ore-ubuntu-image
+  cloud_provider = "aws"
+  iteration_id   = data.hcp_packer_iteration.aws-ubuntu.ulid
+  region         = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.66.0"
+
+  for_each = var.project
+
+  cidr = var.vpc_cidr_block
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = slice(var.private_subnet_cidr_blocks, 0, each.value.private_subnet_count)
+  public_subnets  = slice(var.public_subnet_cidr_blocks, 0, each.value.public_subnet_count)
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+}
+
+module "app_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  for_each = var.project
+
+  name        = "web-server-sg-${each.key}-${each.value.environment}"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = module.vpc[each.key].vpc_id
+
+  ingress_cidr_blocks = module.vpc[each.key].public_subnets_cidr_blocks
+}
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  for_each = var.project
+
+  name = "load-balancer-sg-${each.key}-${each.value.environment}"
+
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id      = module.vpc[each.key].vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "random_string" "lb_id" {
+  length  = 4
+  special = false
+}
+
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "2.4.0"
+
+  for_each = var.project
+
+  # Comply with ELB name restrictions 
+  # https://docs.aws.amazon.com/elasticloadbalancing/2012-06-01/APIReference/API_CreateLoadBalancer.html
+  name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, each.key, each.value.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
+  internal = false
+
+  security_groups = [module.lb_security_group[each.key].this_security_group_id]
+  subnets         = module.vpc[each.key].public_subnets
+
+  number_of_instances = length(module.standard-instance[each.key].instance_ids)
+  instances           = module.standard-instance[each.key].instance_ids
+
+  listener = [{
+    instance_port     = "80"
+    instance_protocol = "HTTP"
+    lb_port           = "80"
+    lb_protocol       = "HTTP"
+  }]
+
+  health_check = {
+    target              = "HTTP:80/index.html"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+  }
+}
+
+resource "aws_instance" "ore-application" {
+  count = var.instance_count
+
+  ami           = data.hcp_packer_image.ubuntu.cloud_image_id
+  instance_type = var.instance_type
+
+  subnet_id              = var.subnet_ids[count.index % length(var.subnet_ids)]
+  vpc_security_group_ids = var.security_group_ids
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
+
+  tags = {
+    Terraform   = "true"
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
+
+
